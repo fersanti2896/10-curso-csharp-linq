7c8924e0cb3cbb0dc3b91926ede90fb5d85cc808
Fernando Nicolás <fersanti2896@gmail.com>
2022-12-01 14:38:51 -0600

    Introducción a LINQ en Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("¡LINQ!");
+
+int[] numeros = Enumerable.Range(0, 20)
+                          .ToArray();
+
+/* Sentencia Linq */
+var pares = numeros.Where(n => n % 2 == 0)
+                   .ToList();
+
+foreach (var par in pares) {
+    Console.Write($"{par} ");
+}
+
+Console.WriteLine();
+
+/* Por Query */
+var pares2 = (from n in numeros
+              where n % 2 == 0
+              select n).ToList();
+
+foreach (var par in pares2) {
+    Console.Write($"{par} ");
+}
